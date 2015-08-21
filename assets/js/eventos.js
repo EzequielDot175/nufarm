fff40e99664795778c1d080938d3a87bb96ad6da
Giannina Calcagno <giannina@dot175.com>
2015-08-21 11:13:55 -0300

    agregando historial

--- assets/js/eventos.js
+++ assets/js/eventos.js
@@ -1,21 +1,25 @@
-(function($){
-
-	$.fn.galeria = function(){
-
-
-	}
-
-})(jQuery);
 
 $(document).ready(function(){
 
-	
+	$(window).resize(function() {
+		var windowHeight = $(window).height();
+		if(windowHeight > 1000){
+			$('.footer').css('position', 'fixed');
+			$('.contenedor-encuesta').css('padding-bottom', '130px');
+		}else{
+			$('.footer').css('position', 'relative');
+			$('.contenedor-encuesta').css('padding-bottom', '130px');
+		}
+	});
+
+	var windowHeight = $(window).height();
+	console.log(windowHeight);
+	if(windowHeight > 1000){
+		$('.footer').css('position', 'fixed');
+	}else{
+		$('.footer').css('position', 'relative');
+	}
     
 });
 
 
- function nombre(current){
-
-	
-
-}
